32706c6de3cdf5ff052168815e3b76ffcc3b6e1c
gar <g-ar@users.noreply.github.com>
2016-10-02 22:59:44 +0530

    first sonic pi program to see the face of github

---
+++ anthem.rb
@@ -0,0 +1,68 @@
+# Indian national anthem, notes taken from one of the websites returned by search engines!
+# Here, to be precise, from https://samarthsongnotes.blogspot.in/2011/06/jana-gana-mana-national-anthem-india.html
+# Need to fine tune the timing, experiment with different chords, etc.
+
+use_bpm 160
+use_synth :pluck
+
+with_fx :reverb do
+  play_pattern [
+    :C, :D, :E, :E, :E, :E, :E, :E, :E, :E, :D, :E, :F
+  ]
+  sleep 0.5
+  play_pattern [
+    :E, :E, :E, :D, :D, :B, :D, :C
+  ]
+  sleep 0.5
+  play_pattern [
+    :C, :G, :G, :G, :G, :G, :G, :G, :F, :A, :G
+  ]
+  sleep 0.5
+  play_pattern [
+    :F, :F, :F, :E, :E, :E, :D, :F, :E
+  ]
+  sleep 0.5
+  play_pattern [
+    :E, :E, :E, :E, :D, :G, :G, :F, :F
+  ]
+  sleep 0.5
+  play_pattern [
+    :E, :E, :E, :D, :D, :D, :B, :D, :C
+  ]
+  sleep 0.5
+  play_pattern [
+    :C, :D, :E, :E, :E, :E, :D, :E, :F
+  ]
+  sleep 0.5
+  play_pattern [
+    :E, :F, :G, :G, :G, :G, :D, :D, :F, :E
+  ]
+  sleep 0.5
+  play_pattern [
+    :E, :E, :D, :D, :D, :D, :B, :D, :C
+  ]
+  sleep 0.5
+  play_pattern [
+    :G, :G, :G, :G, :G, :G, :G, :G, :G, :G, :F, :A, :G
+  ]
+  sleep 0.5
+  play_pattern [
+    :F, :F, :F, :F, :F, :E, :D, :F, :E
+  ]
+  sleep 0.5
+  play_pattern [
+    :C5, :B, :C5
+  ]
+  sleep 0.5
+  play_pattern [
+    :B, :A, :B
+  ]
+  sleep 0.5
+  play_pattern [
+    :A, :G, :A
+  ]
+  sleep 0.5
+  play_pattern [
+    :C, :C, :D, :D, :E, :E, :D, :E, :F
+  ]
+end
